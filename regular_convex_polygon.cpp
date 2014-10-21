2fe7a7345df3a70853d77aa5ce531bc44dc049d6
Adrià Garriga <adria.garriga@gmail.com>
2014-10-21 08:44:52 +0200

    Regular convex polygon solved

--- regular_convex_polygon.cpp
+++ regular_convex_polygon.cpp
@@ -12,23 +12,23 @@ struct Vec {
 
 	Vec() {;}
 	Vec(N a, N b) { x = a; y = b; }
-	N operator* (Vec b) { return x*b.x + y*b.y; }
-	Vec operator* (N a) { return Vec(x*a, y*a); }
-	Vec operator- (Vec b) { return Vec(x-b.x, y-b.y); }
-	Vec operator+ (Vec b) { return Vec(x+b.x, y+b.y); }
 };
 
 static N mod(Vec a) { return sqrtl(a.x*a.x + a.y*a.y); }
 static Vec perp(Vec a) { return Vec(-a.y, a.x); }
+N dot (Vec a, Vec b) { return a.x*b.x + a.y*b.y; }
+Vec scale (Vec a, N n) { a.x*=n; a.y*=n; return a; }
+Vec sub (Vec a, Vec b) { a.x-=b.x; a.y-=b.y; return a; }
+Vec add (Vec a, Vec b) { a.x+=b.x; a.y+=b.y; return a; }
 
 static Vec
 calc_center(Vec a, Vec b, Vec c)
 {
-	Vec abm = (a+b)*.5L;
-	Vec bcm = (b+c)*.5L;
-	Vec v2 = c-b;
-	Vec v1 = a-b;
-	return (v1 * (((bcm-abm)*v2)/(v2*v1))) + abm;
+	Vec abm = scale(add(a, b), .5L);
+	Vec bcm = scale(add(b, c), .5L);
+	Vec v2 = scale(sub(c, b), .5L);
+	Vec v1 = perp(scale(sub(a, b), .5L));
+	return add(scale(v1, dot(sub(bcm, abm), v2)/dot(v2, v1)), abm);
 }
 
 int
@@ -39,14 +39,13 @@ main()
 
 	while(scanf("%Lf %Lf %Lf %Lf %Lf %Lf", &a.x, &a.y, &b.x, &b.y, &c.x, &c.y) == 6) {
 		cen = calc_center(a, b, c);
-		printf("%Lf %Lf\n", cen.x, cen.y);
-		diam = mod(cen-a)*2.L;
-		angab = asinl(fminl(mod(a-b)/diam, 1.L));
-		angbc = asinl(fminl(mod(b-c)/diam, 1.L));
+		diam = mod(sub(cen, a))*2.L;
+		angab = asinl(fminl(mod(sub(a, b))/diam, 1.L));
+		angbc = asinl(fminl(mod(sub(b, c))/diam, 1.L));
 		for(int i=3; i<=1000; i++) {
 			diam = M_PI / ((N)i);
-			remab -= roundl(angab / diam) * diam;
-			rembc -= roundl(angbc / diam) * diam;
+			remab = angab - roundl(angab / diam) * diam;
+			rembc = angbc - roundl(angbc / diam) * diam;
 			if(IS_ZERO(remab) && IS_ZERO(rembc)) {
 				printf("%d\n", i);
 				break;
