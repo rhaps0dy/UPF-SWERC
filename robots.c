d3502b606918fc4bf4e9e5e870c374c68f26618b
Adrià Garriga <adria.garriga@gmail.com>
2014-11-05 11:55:38 +0100

    Solved sentry robots

--- robots.c
+++ robots.c
@@ -1,22 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_N (100*100)
-
-enum robot {
-	NONE,
-	VERT,
-	HOR
-};
+#define MAX_W 100
+#define MAX_H 100
+#define MAX_N (MAX_W*MAX_H)
 
 typedef struct {
 	int x, y, is_obstacle;
-	enum robot robot;
 } Point;
 
 Point *vertorder[MAX_N], *hororder[MAX_N];
 Point points[MAX_N];
-int npoints;
+Point pospoints[MAX_N];
+int npoints, npossiblerobots, w, h;
+Point verts[MAX_H][MAX_W], hors[MAX_H][MAX_W];
+Point verts2[MAX_H][MAX_W], hors2[MAX_H][MAX_W];
+
+#define TRUE_VALUE(p) ( (p).is_obstacle ? hors[(p).y][(p).x].x : verts[(p).y][(p).x].x )
+
+static int
+calc_response(void)
+{
+	int nrobs, i, x, y;
+
+	nrobs = 0;
+	for(i=0; i<npossiblerobots; i++) {
+		if(TRUE_VALUE(pospoints[i]) <= 0) continue;
+		nrobs++;
+/*		PRINT_ALL(); */
+		if(pospoints[i].is_obstacle) {
+			/* horizontal */
+			x = pospoints[i].x;
+			y = pospoints[i].y;
+			hors[y][x].x = 0;
+			/* eliminate this line from records */
+			for(; x<w && hors[y][x].is_obstacle != 1; x++) {
+				if(hors[y][x].is_obstacle == 2)
+					verts[verts[y][x].y][x].x--;	
+				verts[y][x].is_obstacle = 0;
+				hors[y][x].is_obstacle = 0;
+			}
+		} else {
+			x = pospoints[i].x;
+			y = pospoints[i].y;
+			verts[y][x].x = 0;
+			/* eliminate this line from records */
+			for(; y<h && verts[y][x].is_obstacle != 1; y++) {
+				if(verts[y][x].is_obstacle == 2)
+					hors[y][hors[y][x].y].x--;	
+				hors[y][x].is_obstacle = 0;
+				verts[y][x].is_obstacle = 0;
+			}
+		}
+	}
+
+	return nrobs;
+}
+
+static void
+PRINT_ALL(void)
+{
+	int i, x, y;
+
+	puts("sequences");
+	for(i=0; i<npossiblerobots; i++)
+		printf("(%d %d %d %d) ", pospoints[i].x, pospoints[i].y, TRUE_VALUE(pospoints[i]), pospoints[i].is_obstacle);
+	putchar('\n');
+
+	puts("verts\n");
+	for(y=0; y<h; y++) {
+		for(x=0; x<w; x++) {
+			printf("(%d %d %d) ", verts[y][x].x, verts[y][x].y, verts[y][x].is_obstacle);
+		}
+		putchar('\n');
+	}
+
+	puts("hors\n");
+	for(y=0; y<h; y++) {
+		for(x=0; x<w; x++) {
+			printf("(%d %d %d) ", hors[y][x].x, hors[y][x].y, hors[y][x].is_obstacle);
+		}
+		putchar('\n');
+	}
+
+}
 
 static int
 compar_vert(const void *_a, const void *_b)
@@ -46,75 +114,133 @@ compar_hor(const void *_a, const void *_b)
 	return 1;
 }
 
+static int
+compar_seqsizehor(const void *_a, const void *_b)
+{
+	int ta, tb;
+	Point *a, *b;
+
+	a = (Point *)_a;
+	b = (Point *)_b;
+
+	ta = TRUE_VALUE(*a);
+	tb = TRUE_VALUE(*b);
+
+	if(ta > tb) return -1;
+	if(ta < tb) return 1;
+	if(a->is_obstacle < b->is_obstacle) return -1;
+	if(a->is_obstacle > b->is_obstacle) return 1;
+	return 0;
+}
+
+static int
+compar_seqsizevert(const void *_a, const void *_b)
+{
+	int ta, tb;
+	Point *a, *b;
+
+	a = (Point *)_a;
+	b = (Point *)_b;
+
+	ta = TRUE_VALUE(*a);
+	tb = TRUE_VALUE(*b);
+
+	if(ta > tb) return -1;
+	if(ta < tb) return 1;
+	if(a->is_obstacle < b->is_obstacle) return 1;
+	if(a->is_obstacle > b->is_obstacle) return -1;
+	return 0;
+}
+
 static void
 doit(void)
 {
-	int i, j, nrobots, nlines;
-	Point **p;
+	int i, x, y, nrobs2, nrobs1;
 
 	/* width, height */
-	scanf("%*d %*d");
+	scanf("%d %d", &h, &w);
 	/* points of interest */
 	scanf("%d", &npoints);
 	for(i=0; i<npoints; i++) {
 		scanf("%d %d", &points[i].y, &points[i].x);
+		points[i].y--;
+		points[i].x--;
 		points[i].is_obstacle = 0;
-		points[i].robot = NONE;
 		vertorder[i] = hororder[i] = &points[i];
 	}
 
 	/* obstacles */
-	scanf("%d", &i);
-	npoints += i;
+	scanf("%d", &x);
+	npoints += x;
 	for(; i<npoints; i++) {
 		scanf("%d %d", &points[i].y, &points[i].x);
+		points[i].y--;
+		points[i].x--;
 		points[i].is_obstacle = 1;
-		points[i].robot = NONE;
 		vertorder[i] = hororder[i] = &points[i];
 	}
 
 	qsort(vertorder, npoints, sizeof(Point *), compar_vert);
 	qsort(hororder, npoints, sizeof(Point *), compar_hor);
+	memset(hors, 0, sizeof(Point)*MAX_H*MAX_W);
+	memset(verts, 0, sizeof(Point)*MAX_H*MAX_W);
 
-	puts("Horizontal order:");
-	for(i=0; i<npoints; i++)
-		printf("(%d %d %d) ", hororder[i]->x, hororder[i]->y, hororder[i]->is_obstacle);
-	putchar('\n');
-
-	puts("Vertical order:");
-	for(i=0; i<npoints; i++)
-		printf("(%d %d %d) ", vertorder[i]->x, vertorder[i]->y, vertorder[i]->is_obstacle);
-	putchar('\n');
-
-	nrobots = 0;
-	if(!hororder[0]->is_obstacle) {
-		hororder[0]->robot = HOR;
-		nrobots++;
-	}
-	for(i=1; i<npoints; i++) {
-		if(hororder[i-1]->is_obstacle) {
-			hororder[i]->robot = HOR;
-			nrobots++;
+	npossiblerobots = 0;
+	
+	/* create horizontal grid with info of all possible robots */
+	for(i=0; i<npoints; ) {
+		x = hororder[i]->x;
+		y = hororder[i]->y;
+		for(; i<npoints && !hororder[i]->is_obstacle && hororder[i]->y == y; i++) {
+			hors[y][x].x ++;
+			hors[y][hororder[i]->x].y = x;
+			hors[y][hororder[i]->x].is_obstacle = 2;
 		}
+		pospoints[npossiblerobots].x = x;
+		pospoints[npossiblerobots].y = y;
+		pospoints[npossiblerobots].is_obstacle = 1;
+		npossiblerobots++;
+		for(; i<npoints && hororder[i]->is_obstacle; i++)
+			hors[hororder[i]->y][hororder[i]->x].is_obstacle = 1;
 	}
 
-	if(!vertorder[0]->is_obstacle) {
-		if(vertorder[0]->robot == NONE) {
-			/* if putting a vertical line here eliminates one or more horizontal
-			 * lines, do it */
-			nlines = 0;
-			for(j=0; !vertorder[j].is_obstacle && j<npoints; j++) {
-				p = (Point **) bsearch(vertorder[j], hororder, sizeof(Point *), npoints, hor_compar)
-				/* TOCONTIUE */
-			}
-		} else {
+	/* vertical */
+	for(i=0; i<npoints; ) {
+		x = vertorder[i]->x;
+		y = vertorder[i]->y;
+		for(; i<npoints && !vertorder[i]->is_obstacle && vertorder[i]->x == x; i++) {
+			verts[y][x].x ++;
+			verts[vertorder[i]->y][x].y = y;
+			verts[vertorder[i]->y][x].is_obstacle = 2;
 		}
+		pospoints[npossiblerobots].x = x;
+		pospoints[npossiblerobots].y = y;
+		pospoints[npossiblerobots].is_obstacle = 0;
+		npossiblerobots++;
+		for(; i<npoints && vertorder[i]->is_obstacle; i++)
+			verts[vertorder[i]->y][vertorder[i]->x].is_obstacle = 1;
 	}
+
+	memcpy(verts2, verts, sizeof(Point)*MAX_W*MAX_H);
+	memcpy(hors2, hors, sizeof(Point)*MAX_W*MAX_H);
+	qsort(pospoints, npossiblerobots, sizeof(Point), compar_seqsizevert);
+	nrobs1 = calc_response();
+
+	memcpy(verts, verts2, sizeof(Point)*MAX_W*MAX_H);
+	memcpy(hors, hors2, sizeof(Point)*MAX_W*MAX_H);
+	qsort(pospoints, npossiblerobots, sizeof(Point), compar_seqsizehor);
+	nrobs2 = calc_response();
+
+	printf("%d\n", (nrobs1 < nrobs2 ? nrobs1 : nrobs2));
 }
 
 int
 main()
 {
-	doit();
+	int n, i;
+
+	scanf("%d", &n);
+	for(i=0; i<n; i++)
+		doit();
 	return 0;
 }
