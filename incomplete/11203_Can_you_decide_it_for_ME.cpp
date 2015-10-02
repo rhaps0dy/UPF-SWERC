41837f0c1488de4f319d2ec66227cbe996849a43
drako9 <jorge.alemangonzalez@gmail.com>
2015-10-02 16:28:51 +0200

    jorge commit

--- incomplete/11203_Can_you_decide_it_for_ME.cpp
+++ incomplete/11203_Can_you_decide_it_for_ME.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
-
+#include<stdio.h>
 #include<string>
-
 using namespace std;
 
-void main(){
-    int n;
+int main(){
+    int n ;
     string s;
     cin>>n;
-    while(n>0)
+    while(n > 0)
     {
-    scanf("%s",s);
-    cout<<"hola"<<endl;
-    --n;
+        bool ME = true;
+        cin>>s;
+        for(int i = 0 ; i < s.size() ; ++i)
+        {
+            if(s[i] != '?' || s[i] != 'M' || s[i] != 'E')
+            {
+                ME = false;
+                break;
+            }
+
+        }
+        --n;
     }
 }
