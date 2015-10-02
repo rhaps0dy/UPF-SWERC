a21ca3146fa5dc6110e9ff31fa3d38eae745eeb0
drako9 <jorge.alemangonzalez@gmail.com>
2015-10-02 16:18:09 +0200

    Strings1

---
+++ incomplete/11203_Can_you_decide_it_for_ME.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+void main(){
+    int n;
+    string s;
+    cin>>n;
+    while(n>0)
+    {
+    scanf("%s",s);
+    cout<<"hola"<<endl;
+    --n;
+    }
+}
